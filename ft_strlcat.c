dfa795901c55d58af6e7b93bcf5223f4234b9215
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-06-18 19:16:34 +0000

    fix: strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 22:24:48 by pchung            #+#    #+#             */
-/*   Updated: 2024/06/18 15:19:46 by pchung           ###   ########.fr       */
+/*   Updated: 2024/06/18 19:15:58 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,20 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t i=0;
-	size_t j=0;
-	size_t dstlen=0;
-	size_t srclen=0;
-	size_t size=dstsize;
+	size_t	dstlen;
+	size_t	srclen;
+	size_t	i;
 
-	while(src[srclen] != '\0')
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstsize <= dstlen)
+		return (srclen + dstsize);
+	i = 0;
+	while (src[i] && dstlen + i < dstsize - 1)
 	{
-		srclen++;
+		dst[dstlen + i] = src[i];
+		i++;
 	}
-	if(dst == NULL && dstsize == 0)
-	{
-		return (srclen);
-	}
-	while (dst[dstlen] != '\0')
-	{
-		dstlen ++;
-	}
-	while (dstsize > dstlen)
-	{
-		dst[dstlen + i] = src[j];
-		i ++;
-		j ++;
-		dstsize --;
-	}
-	return (srclen + size);
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
 }
