df12ad8be0f0c6d0cc10cde9649e9b6aad748473
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-06-18 16:42:13 +0000

    fix: substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 22:25:29 by pchung            #+#    #+#             */
-/*   Updated: 2024/06/18 15:37:53 by pchung           ###   ########.fr       */
+/*   Updated: 2024/06/18 16:35:36 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (start >= ft_strlen(s))
 	{
-		substr = (char *)malloc(1);
+		substr = (char *) ft_calloc(1, sizeof(char));
 		if (!substr)
 			return (NULL);
-		substr[0] = '\0';
 		return (substr);
 	}
 	if (len > ft_strlen(s) - start)
 		len = ft_strlen(s) - start;
-	substr = (char *) malloc(len + 1);
+	substr = (char *) ft_calloc(len + 1, sizeof(char));
 	if (!substr)
 		return (NULL);
 	i = 0;
@@ -38,6 +37,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		substr[i] = s[start + i];
 		i++;
 	}
-	substr[i] = '\0';
 	return (substr);
 }
