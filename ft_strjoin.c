cd00ca199a00d3c0d4a80932a8a5542eb20952c7
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-06-18 18:51:22 +0000

    fix: strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 22:24:43 by pchung            #+#    #+#             */
-/*   Updated: 2024/06/18 16:15:41 by pchung           ###   ########.fr       */
+/*   Updated: 2024/06/18 18:50:29 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
 	size_t	s1len;
 	size_t	s2len;
 	char	*newstr;
 
-	i = 0;
-	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
 	s1len = ft_strlen(s1);
@@ -29,16 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	newstr = (char *) malloc(s1len + s2len + 1);
 	if (!newstr)
 		return (NULL);
-	while (i < s1len)
-	{
-		newstr[i] = s1[i];
-		i++;
-	}
-	while (j <= s2len)
-	{
-		newstr[i + j] = s2[j];
-		j++;
-	}
-	newstr[i + j] = '\0';
+	ft_strlcpy(newstr, s1, s1len + 1);
+	ft_strlcpy(newstr + s1len, s2, s2len + 1);
 	return (newstr);
 }
