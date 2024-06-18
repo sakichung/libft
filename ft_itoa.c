cc6aeeb6d0cdd653160689c96d81a17c483496e4
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-06-18 19:04:42 +0000

    fix: itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 22:23:27 by pchung            #+#    #+#             */
-/*   Updated: 2024/06/18 14:59:54 by pchung           ###   ########.fr       */
+/*   Updated: 2024/06/18 19:03:00 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,29 +35,21 @@ char	*ft_itoa(int n)
 	size_t			i;
 
 	len = get_num_len(n);
-	str = (char *)malloc(len + 1);
+	str = (char *)ft_calloc(sizeof(char), len + 1);
 	if (!str)
 		return (NULL);
-	str[len] = '\0';
+	str[0] = '-';
 	if (n < 0)
-	{
-		str[0] = '-';
 		num = -n;
-	}
 	else
-	{
 		num = n;
-	}
 	i = len - 1;
-	if (num == 0)
-	{
-		str[0] = '0';
-		return (str);
-	}
-	while (num > 0)
+	while (1)
 	{
 		str[i] = (num % 10) + '0';
 		num /= 10;
+		if (num == 0)
+			break ;
 		i --;
 	}
 	return (str);
